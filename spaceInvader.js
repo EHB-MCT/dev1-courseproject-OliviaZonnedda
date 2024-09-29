d6f3a983a49aa1ff8330006238bf8bea9d3e6140
Olivia Zonnedda <olivia.zonnedda@student.ehb.be>
2024-09-29 19:38:03 +0200

    spaceInvader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,90 @@
-"use strict";
+draw();
 
-alert("Draw your space invader here");
+function draw(){
+
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+    drawSignature();
+
+
+
+
+    function drawSignature(){
+
+        //base
+
+        context.strokeStyle = "black";
+    context.fillStyle = "black";
+
+    context.beginPath();
+    context.rect(50,50,300,300);
+
+    context.fill();
+    context.stroke();
+    
+    //spaceInvader
+context.strokeStyle = "salmon";
+context.fillStyle = "salmon";
+//upper piece
+context.beginPath();
+context.rect(125,70,150,50);
+
+context.fill();
+context.stroke();
+
+//horizontal right
+context.beginPath();
+context.rect(225,120,100,50);
+
+context.fill();
+context.stroke();
+
+//horizontal left
+context.beginPath();
+context.rect(75,120,100,50);
+
+context.fill();
+context.stroke();
+
+//vertical left
+context.beginPath();
+context.rect(75,120,50,150);
+
+context.fill();
+context.stroke();
+
+//vertical right
+context.beginPath();
+context.rect(275,120,50,150);
+
+context.fill();
+context.stroke();
+
+//lower left
+context.beginPath();
+context.rect(125,270,50,50);
+
+context.fill();
+context.stroke();
+
+//lower middle
+context.beginPath();
+context.rect(175,220,50,50);
+
+context.fill();
+context.stroke();
+
+//lower right
+context.beginPath();
+context.rect(225,270,50,50);
+
+context.fill();
+context.stroke();
+
+    }
+
+}
+
